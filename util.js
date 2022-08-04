47dbba361c8aef64b81220095aa2208e16a61e81
Danijel <31479093+ethernal12@users.noreply.github.com>
2022-08-04 19:42:57 +0200

    Adding checkAndGenerate helper function, adding UNIT and integration tests. Removing validate inputs from app.js and moving outputText validator.

--- util.js
+++ util.js
@@ -19,8 +19,20 @@ exports.validateInput = (text, notEmpty, isNumber) => {
   if (notEmpty && text.trim().length === 0) {
     return false;
   }
+ 
   if (isNumber && +text === NaN) {
     return false;
   }
   return true;
+};
+
+exports.checkAndGenerate = (name, age) => {
+  if (
+    !this.validateInput(name, true, false) ||
+    !this.validateInput(age, false, true)
+  ) {
+    return false;
+  }
+  return this.generateText(name, age);
+
 };
