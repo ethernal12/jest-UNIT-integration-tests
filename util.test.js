3e407f71c30e0df192b37ba97ae5364bc31f2f68
Danijel <korbar41@gmail.com>
2022-08-05 08:20:00 +0200

    Adding describe to tests.

--- util.test.js
+++ util.test.js
@@ -19,32 +19,40 @@ test('should generate valid text-output', () => {
     expect(text).toBe('danijel (40 years old)');
 });
 
-test('should validate name input', () => {
 
-    const validateText = validateInput('dani', true, true);
+describe('UNIT testing', () => {
+    test('should validate name input', () => {
 
-    expect(validateText).toBe(true);
+        const validateText = validateInput('dani', true, true);
 
+        expect(validateText).toBe(true);
+
+    });
+    test('should validate age input', () => {
+
+        const validateNumber = validateInput(23, false, true);
+
+        expect(validateNumber).toBe(true);
+    });
 });
-test('should validate age input', () => {
 
-    const validateNumber = validateInput(23, false, true);
 
-    expect(validateNumber).toBe(true);
+
+describe('E2E testing', () => {
+    test('should click around', async () => {
+        const browser = await puppeteer.launch({
+            headless: true,
+            slowMo: 80,
+            args: ['--window-size=1920,1080']
+        })
+        const page = await browser.newPage();
+        await page.goto('http://127.0.0.1:5500/index.html');
+        await page.click('input#name');
+        await page.type('input#name', 'danijel');
+        await page.type('input#age', '40');
+        await page.click('#btnAddUser');
+        const finalText = await page.$eval('.user-item', el => el.textContent);
+        expect(finalText).toBe('danijel (40 years old)');
+    }, 10000);
 });
 
-test('should click around', async () => {
-    const browser = await puppeteer.launch({
-        headless: true,
-        slowMo: 80,
-        args: ['--window-size=1920,1080']
-    })
-    const page = await browser.newPage();
-    await page.goto('http://127.0.0.1:5500/index.html');
-    await page.click('input#name');
-    await page.type('input#name', 'danijel');
-    await page.type('input#age', '40');
-    await page.click('#btnAddUser');
-    const finalText = await page.$eval('.user-item', el => el.textContent);
-    expect(finalText).toBe('danijel (40 years old)');
-}, 10000);
